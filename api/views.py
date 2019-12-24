4dfed66f269db7a46b730237f4951b8a3beb3fb7
ishmam-hossain <ishmam.hossain101111@gmail.com>
2019-12-25 00:53:24 +0600

    tweaked a bit

--- api/views.py
+++ api/views.py
@@ -1,8 +1,11 @@
-from rest_framework.response import Response
-from rest_framework import status
-from rest_framework.views import APIView
 from django.conf import settings
-from common.utils import (get_splitted_list_from_string, get_redis_data, get_key)
+from rest_framework import status
+from rest_framework.response import Response
+from rest_framework.views import APIView
+
+from common.utils import (get_key,
+                          get_redis_data,
+                          string_splitter, )
 
 redb = settings.REDIS_DB
 
@@ -14,15 +17,14 @@ class ValuesAPIView(APIView):
         key_args = self.request.GET.get('keys')
 
         if key_args:
-            all_keys = get_splitted_list_from_string(
+            all_keys = string_splitter(
                 source_str=key_args,
                 split_by=',',
                 prefix=self.PREFIX
-            )
+            )   # list -> iterable
         else:
-            all_keys = list(redb.scan_iter(f"{self.PREFIX}*"))
+            all_keys = redb.scan_iter(f"{self.PREFIX}*")    # generator object -> iterable
 
-        #   TODO: handle non existent data
         response = {get_key(key): get_redis_data(redb=redb, key=key) for key in all_keys}
 
         return Response(
--- common/utils.py
+++ common/utils.py
@@ -1,4 +1,4 @@
-def get_splitted_list_from_string(source_str: str, split_by: str, prefix=None) -> list:
+def string_splitter(source_str: str, split_by: str, prefix=None) -> list:
     splitted_list = source_str.split(split_by)
     if prefix:
         return [f"{prefix}_{val}" for val in splitted_list]
@@ -6,7 +6,7 @@ def get_splitted_list_from_string(source_str: str, split_by: str, prefix=None) -
 
 
 def get_key(_key):
-    return get_splitted_list_from_string(_key, '_')[1]
+    return string_splitter(_key, '_')[1]
 
 
 def get_redis_data(redb, key):
