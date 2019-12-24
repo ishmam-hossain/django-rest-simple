80fe129550bfbce9a092f45ad8c69a3aa682c884
ishmam-hossain <ishmam.hossain101111@gmail.com>
2019-12-24 22:24:12 +0600

    basic api view done

--- api/views.py
+++ api/views.py
@@ -8,13 +8,13 @@ redb = settings.REDIS_DB
 
 class ValuesAPIView(APIView):
     def get(self, request):
-        print("************************")
-        print(settings.REDIS_HOST)
-        print(settings.REDIS_PORT)
-        print("************************")
-        all_keys = redb.get("me")
-        print(all_keys)
-        print("************************")
+        key_args = self.request.GET.get('keys')
+        if key_args:
+            keys_asked_for = key_args.split(',')
+
+        all_keys = redb.scan_iter("values*")
+        for k in all_keys:
+            print(k)
 
         return Response(
             {
@@ -23,3 +23,25 @@ class ValuesAPIView(APIView):
             },
             status=status.HTTP_200_OK
         )
+
+    def post(self, request):
+        from ast import literal_eval
+        data = literal_eval(self.request.body.decode())
+        insert_prefix = 'values'
+
+        for key in data:
+            try:
+                redb.set(f"{insert_prefix}_{key}", data[key])
+            except Exception as e:
+                print(e)
+
+        return Response(
+            {
+                "status": "ok",
+                "message": "success",
+            }
+        )
+
+    def patch(self, request):
+        pass
+
