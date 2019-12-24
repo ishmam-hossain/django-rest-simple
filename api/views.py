a0fea4887a63d66c15ee33a865e9f4e50d57baed
ishmam-hossain <ishmam.hossain101111@gmail.com>
2019-12-24 21:33:21 +0600

    added .env file holding redis info

--- api/views.py
+++ api/views.py
@@ -1,8 +1,13 @@
 from rest_framework.response import Response
 from rest_framework import status
 from rest_framework.views import APIView
+from django.conf import settings
 
 
 class ValuesAPIView(APIView):
     def get(self, request):
+        print("************************")
+        print(settings.REDIS_HOST)
+        print(settings.REDIS_PORT)
+        print("************************")
         return Response({"ok": 1}, status=status.HTTP_200_OK)
