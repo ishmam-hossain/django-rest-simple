da8b51739cf3f96370d185ce4a02ae9d425f5c70
ishmam-hossain <ishmam.hossain101111@gmail.com>
2019-12-25 21:38:22 +0600

    updated decorator to handle invalid json with POST & PATCH

--- common/decorators.py
+++ common/decorators.py
@@ -1,15 +1,24 @@
 from rest_framework.response import Response
 from rest_framework import status
+from json import JSONDecodeError
 
 
 def valid_request(f):
     def decorated_func(self, request, *args, **kwargs):
-        if self.request.data:
-            return f(self, request, *args, **kwargs)
-        else:
+        try:
+            _ = self.request.data
+        except (JSONDecodeError, Exception) as e:
+            return Response({
+                "status": "fail",
+                "message": "invalid json"
+            }, status=status.HTTP_400_BAD_REQUEST)
+
+        if not self.request.data:
             return Response({
                 "status": "fail",
                 "message": "json not provided"
             }, status=status.HTTP_400_BAD_REQUEST)
+
+        return f(self, request, *args, **kwargs)
 
     return decorated_func
